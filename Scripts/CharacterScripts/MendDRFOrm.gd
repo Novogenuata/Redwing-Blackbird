3a0d6f34cece04a5c84526e25dbcbc02ee241591
Sigourney <2250485@crosemont.quebec.ca>
2025-12-07 17:18:57 -0500

    added a firing animation

--- Scripts/CharacterScripts/MendDRFOrm.gd
+++ Scripts/CharacterScripts/MendDRFOrm.gd
@@ -1,2 +1,25 @@
 
 extends player_script
+var firing = false
+
+func _physics_process(delta: float) -> void:
+	
+	if firing:
+		velocity.x = 0
+		move_and_slide()
+		return
+		
+	super(delta)
+	if Input.is_action_pressed("Fire"):
+		firing = true
+		Sprite.play("Fire")
+		
+		
+	
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if Sprite.animation == "Fire":
+		firing = false
+		
+		
